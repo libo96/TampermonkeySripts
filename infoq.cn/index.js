3940f2b84774b188e94a4430397a303b2586147f
libo <thunderstorm96@icloud.com>
2022-09-08 10:21:23 +0800

    Update index.js

--- infoq.cn/index.js
+++ infoq.cn/index.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.infoq.cn/*
+// @match       https://*.infoq.cn/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
@@ -14,6 +14,13 @@
     'use strict';
 
     setInterval(function(){
+
+        var msg = document.querySelector("#layout > div.layout-header-wrap > div > div.header > div.wrap > div > div.header-middle > div.message")
+
+        if(msg != undefined){
+            msg.remove()
+        }
+
         var e = document.querySelector("#layout > div.live-fixed-bar")
         if(e != undefined){
             e.remove();
@@ -34,41 +41,36 @@
         if(e4 != undefined){
             e4.remove();
         }
-        
+
         var square = document.querySelector("#layout > div.inner-content > div.ActivityFloatImg_float-image_2kMyt")
         if(square != undefined){
             square.remove()
         }
-        
+
         var head = document.querySelector("#layout > div.common-header-wap > div.header-container")
         if(head != undefined){
             head.remove()
         }
-        
+
         var headNotice = document.querySelector("#layout > div.header.common-header-pc.layout-header.had-notice > div.header-notice")
-        
+
         if(headNotice != undefined){
             headNotice.remove()
         }
-        
+
         var message = document.querySelector("#layout > div.header.common-header-pc.layout-header.had-notice > div.layout-content.header-content > div.message")
-        
+
         if(message != undefined){
             message.remove()
         }
-        
-        var articleMsg = document.querySelector("#layout > div.layout-header-wrap > div > div.header > div.wrap > div > div.header-middle > div.message")
 
-        if(articleMsg != undefined){
-            articleMsg.remove()
-        }
-        
+
         document.querySelector("#layout > div.live-fixed-bar > div.live-bar > div > div").remove();
          document.querySelector("#layout > div.inner-content.had-notice > div.widget-slide-weekly").remove();       // 移除个人介绍栏
         document.querySelector("#layout > div.live-fixed-bar").remove();   // 删除侧边栏滚动条
-         
+
         document.querySelector("#layout > div.header.common-header-pc.layout-header > div.layout-content.header-content > div.message").remove();   // 移除消息栏，通知小红标
-        
+
 
 
     }, 2000 );
